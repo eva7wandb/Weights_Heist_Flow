daea25a2e34978c3e49a75c4b1866e8356463b6c
sdev2030 <subrananjun@gmail.com>
2021-11-28 10:59:56 +0530

    Update data.py
    change cutout with 1 hole of 8x8

--- utils/data.py
+++ utils/data.py
@@ -37,7 +37,8 @@ class CIFAR10_dataset():
                 A.CropAndPad(px=4,keep_size=False, p=0.5,),
                 A.RandomCrop(32, 32, always_apply=False, p=1),
                 A.HorizontalFlip(p=0.5),
-                A.Cutout (num_holes=8, max_h_size=8, fill_value=(0.491, 0.482, 0.447), always_apply=False, p=0.5),
+                #A.Cutout (num_holes=8, max_h_size=8, fill_value=(0.491, 0.482, 0.447), always_apply=False, p=0.5),
+                A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=0, always_apply=False, p=0.5),
 #                 A.CoarseDropout(
 #                     max_holes=3, max_height=16, max_width=16, min_holes=None, min_height=None, min_width=None, 
 #                     fill_value=(0.491, 0.482, 0.447), mask_fill_value=None, always_apply=False, p=0.25
