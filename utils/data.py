dd10feb5353408a42194837bfcc902797383b13c
Muhsin Mohammed <askmuhsin@gmail.com>
2021-11-29 16:10:41 +0530

    aug | refine vals

--- utils/data.py
+++ utils/data.py
@@ -32,13 +32,13 @@ class CIFAR10_dataset():
                 A.Sequential([
                     A.PadIfNeeded(40,40),
                     A.RandomCrop(32,32)],
-                    p=0.9
+                    p=0.75
                 ),
                 #A.CropAndPad(px=4,keep_size=False, p=0.5,),
                 #A.RandomCrop(32, 32, always_apply=False, p=1),
-                A.HorizontalFlip(p=0.8),
+                A.HorizontalFlip(p=0.75),
                 #A.Cutout (num_holes=8, max_h_size=8, fill_value=(0.491, 0.482, 0.447), always_apply=False, p=0.5),
-                A.Cutout(num_holes=3, max_h_size=8, max_w_size=8, fill_value=self.mean, always_apply=False, p=0.8),
+                A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=.4, always_apply=False, p=0.75),
 #                 A.CoarseDropout(
 #                     max_holes=3, max_height=16, max_width=16, min_holes=None, min_height=None, min_width=None, 
 #                     fill_value=self.mean, mask_fill_value=None, always_apply=False, p=0.25
@@ -106,6 +106,6 @@ class CIFAR10_dataset():
         )
         data_loader = DataLoader(data, **dataloader_args)
         print(
-            f"""[INFO] {'train' if self.train else 'test'} dataset of size {len(data)} loaded... (v4.3)"""
+            f"""[INFO] {'train' if self.train else 'test'} dataset of size {len(data)} loaded... (v4.4)"""
         )
         return data_loader
