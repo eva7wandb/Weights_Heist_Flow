642a6afcf649fffa2b1bef0f35bafa52486d0479
Muhsin Mohammed <askmuhsin@gmail.com>
2021-11-29 19:22:02 +0530

    feat | update find_lr

--- main.py
+++ main.py
@@ -135,22 +135,52 @@ class Trainer:
             self.test_loader, self.criterion, epoch=0,
         )
     
-    def find_lr(self):
-        lr_finder = LRFinder(self.net, self.optimizer, self.criterion, device=device)
-        lr_finder.range_test(self.train_loader, end_lr=1, num_iter=100, step_mode="exp")
-        lr_finder.plot()
-        best_loss_ind = lr_finder.history['loss'].index(lr_finder.best_loss)
-        print('few steps before and after best_loss')
-        print('(relative step, lr, loss)')
-        print(*[
-            (n - 5, round(lr, 5), round(loss, 5))
-            for n, (lr, loss) in enumerate(zip(
-                lr_finder.history['lr'][best_loss_ind - 5 : best_loss_ind + 5], 
-                lr_finder.history['loss'][best_loss_ind - 5 : best_loss_ind + 5], 
-            ))
-        ], sep='\n')
-        self.lr_history = lr_finder.history
-        lr_finder.reset()
+    def find_lr(self, init_value=1e-8, final_value=8.0):
+        print(f'Starting Find_LR Algorithm')
+        print(f'initial_LR from model, self.lr is {self.lr}')
+        number_in_epoch = len(self.train_loader) - 1
+        print(f'number_in_epoch is {number_in_epoch}')
+        update_step = (final_value / init_value) ** (1 / number_in_epoch)
+        print(f'update_step is {update_step})
+        self.lr = init_value
+        print(f'after setting int_value, model self.lr is {self.lr}')
+        self.optimizer.param_groups[0]["lr"] = self.lr
+        print(f'Confirming that the self.lr passes onto optimizer.param_groups: {self.optimizer.param_groups[0]["lr"]}')
+        best_loss = 0.0
+        batch_num = 0
+        losses = []
+        log_lrs = []
+        for data in self.train_loader:
+            batch_num += 1
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            self.optimizer.zero_grad()
+            outputs = self.net(inputs)
+            loss = self.criterion(outputs, labels)
+
+            # exit if loss shoots up
+            if batch_num > 1 and loss > 4 * best_loss:
+                return log_lrs[10:-5], losses[10:-5]
+
+            # Record the best loss
+            if loss < best_loss or batch_num == 1:
+                best_loss = loss
+
+            # Store the values
+            losses.append(loss)
+            log_lrs.append(math.log10(self.lr))
+
+            # Do the backward pass and optimize
+            loss.backward()
+            self.optimizer.step()
+
+            # update the lr for next step and store
+            self.lr *= update_step
+            print(f'after update, new model self.lr becomes {self.lr}')
+            
+            self.optimizer.param_groups[0]["lr"] = self.lr
+            print(f'Confirming that the self.lr passes onto optimizer.param_groups: {self.optimizer.param_groups[0]["lr"]}']
+        return log_lrs[10:-5], losses[10:-5]
 
 
 def show_misclassification(trainer, cam_layer_name='layer4'):
