d07365b982c1eebfcd56a82eeeae5ae34a39ece8
Muhsin Mohammed <askmuhsin@gmail.com>
2021-11-29 15:59:08 +0530

    aug | pad, crop, flip

--- utils/data.py
+++ utils/data.py
@@ -32,11 +32,11 @@ class CIFAR10_dataset():
                 A.Sequential([
                     A.PadIfNeeded(40,40),
                     A.RandomCrop(32,32)],
-                    p=0.5
+                    p=0.9
                 ),
                 #A.CropAndPad(px=4,keep_size=False, p=0.5,),
                 #A.RandomCrop(32, 32, always_apply=False, p=1),
-                A.HorizontalFlip(p=0.5),
+                A.HorizontalFlip(p=0.8),
                 #A.Cutout (num_holes=8, max_h_size=8, fill_value=(0.491, 0.482, 0.447), always_apply=False, p=0.5),
                 A.Cutout(num_holes=3, max_h_size=8, max_w_size=8, fill_value=self.mean, always_apply=False, p=0.8),
 #                 A.CoarseDropout(
