49c8db42429ab897e49727ea8b0024056556ed71
Muhsin Mohammed <askmuhsin@gmail.com>
2021-11-29 15:20:47 +0530

    aug | cutout to 0

--- utils/data.py
+++ utils/data.py
@@ -36,9 +36,9 @@ class CIFAR10_dataset():
                 ),
                 #A.CropAndPad(px=4,keep_size=False, p=0.5,),
                 #A.RandomCrop(32, 32, always_apply=False, p=1),
-                A.HorizontalFlip(p=0.3),
+                A.HorizontalFlip(p=0.5),
                 #A.Cutout (num_holes=8, max_h_size=8, fill_value=(0.491, 0.482, 0.447), always_apply=False, p=0.5),
-                A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=self.mean, always_apply=False, p=0.3),
+                A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=0, always_apply=False, p=0.3),
 #                 A.CoarseDropout(
 #                     max_holes=3, max_height=16, max_width=16, min_holes=None, min_height=None, min_width=None, 
 #                     fill_value=self.mean, mask_fill_value=None, always_apply=False, p=0.25
